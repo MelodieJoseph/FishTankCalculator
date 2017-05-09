4a7d0e614d1132add32a700c01be889e1c210ca6
MelodieJoseph <melodiejosartist@gmail.com>
2017-05-09 19:48:33 +1200

    Update Program.cs

--- Section2CC/Program.cs
+++ Section2CC/Program.cs
@@ -8,9 +8,6 @@ namespace Section2CC
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
